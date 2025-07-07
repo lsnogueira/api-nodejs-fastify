c4a845905594f010f316e87c943a46b80daf107f
Lucas Nogueira <lucas_snogueira@hotmail.com>
2025-07-07 15:21:47 -0300

    add transactions list test

--- src/tests/transactions.spec.ts
+++ src/tests/transactions.spec.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import { afterAll, beforeAll, describe, it } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 import { app } from '../app';
 
 describe('Transaction routes', () => {
@@ -20,5 +20,29 @@ describe('Transaction routes', () => {
         type: 'credit',
       })
       .expect(201);
+  });
+
+  it('shoulde be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 500,
+        type: 'credit',
+      })
+      .expect(201);
+
+    const cookie = createTransactionResponse.get('Set-Cookie');
+    const listAllTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie!)
+      .expect(200);
+
+    expect(listAllTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 500,
+      }),
+    ]);
   });
 });
